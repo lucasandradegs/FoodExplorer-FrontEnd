e1b8a0def5f756accd66093274773bed680fb09b
Lucas Andrade <contato.developerlucas@gmail.com>
2023-11-14 17:42:54 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://doce-maria-api.onrender.com"
+    baseURL: "https://docemaria-backend-production.up.railway.app/"
 })

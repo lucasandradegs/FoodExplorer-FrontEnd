b382314887d6ea43c04c5963258775fd5f4641d3
Lucas Andrade <115107945+lucasandradegs@users.noreply.github.com>
2023-11-14 19:57:48 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://docemaria-backend-production.up.railway.app/"
+    baseURL: "https://docemaria-backend-production.up.railway.app"
 })
